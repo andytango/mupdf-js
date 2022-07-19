c4152dcd6d9c4bba9e3df4bb4fdad1a5daade2fa
Andrew Hall <andrew@rioja.io>
2022-07-19 05:53:54 +0000

    chore: update examples for new feature

--- bin/build-examples.ts
+++ bin/build-examples.ts
@@ -11,7 +11,7 @@ async function main() {
   const doc = openDocument("example.pdf");
   const n = countPages(doc);
 
-  ["png", "svg", "html"].forEach((ext) => {
+  ["png", "svg", "html", "text"].forEach((ext) => {
     mkdirSync(`./examples/${ext}`, { recursive: true });
   });
 
@@ -21,6 +21,8 @@ async function main() {
     writePageToPngFile(i, mupdf, doc);
     writePageToSvgFile(i, mupdf, doc);
     writePageToHtmlFile(i, mupdf, doc);
+    writePageToTextFile(i, mupdf, doc);
+    writePageSearchToFile(i, mupdf, doc);
   }
   console.groupEnd();
 }
@@ -38,6 +40,17 @@ function writePageToSvgFile(i: number, { drawPageAsSVG }: any, doc: any) {
 
 function writePageToHtmlFile(i: number, { drawPageAsHTML }: any, doc: any) {
   writeFileSync(`./examples/html/example-${i}.html`, drawPageAsHTML(doc, i));
+}
+
+function writePageToTextFile(i: number, { getPageText }: any, doc: any) {
+  writeFileSync(`./examples/text/example-${i}.txt`, getPageText(doc, i));
+}
+
+function writePageSearchToFile(i: number, { searchPageText }: any, doc: any) {
+  writeFileSync(
+    `./examples/search/example-${i}-search.json`,
+    JSON.stringify(searchPageText(doc, i, 'lorem', 10), null, "  ")
+  );
 }
 
 function decodeUri(uri: string) {
